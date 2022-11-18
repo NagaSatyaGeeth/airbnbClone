844fcdc773e92c8943ad01ad990ec610177d8258
Naga Satya Geeth <67518197+Artistole@users.noreply.github.com>
2022-11-18 17:07:14 +0530

    add props

--- src/components/Cards/card.jsx
+++ src/components/Cards/card.jsx
@@ -3,26 +3,27 @@ import "./card.css";
 import swimmerImg from "/src/assets/image 12.png";
 import star from "/src/assets/Star 1.png";
 
-var cards = () => {
+var cards = (props) => {
 	return (
 		<div className="card-placeholder">
 			<div className="card-info">
-				<div className="card-info-desc">SOLD OUT</div>
+				<div className="card-info-desc">{props.status}</div>
 			</div>
 			<div className="card-pic">
-				<img src={swimmerImg} className="swimmer-pic" />
+				<img src={props.img} className="swimmer-pic" />
 			</div>
 			<div className="rating">
 				<div className="star-pic">
 					<img src={star} />
 				</div>
-				<div className="total-rating">5.0</div>
-				<div className="no-of-reviews">(6)</div>
-				<div className="country">USA</div>
+				<div className="total-rating">{props.rating}</div>
+				<div className="no-of-reviews">{props.noOfReviews}</div>
+				<div className="country">{props.country}</div>
 			</div>
-			<div className="card-title">Life lessons wiht Katie</div>
+			<div className="card-title">{props.cardTitle}</div>
 			<div className="price">
-				From $136 <span className="person"> / person</span>
+				{props.price}
+				<span className="person"> / person</span>
 			</div>
 		</div>
 	);
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,12 +3,44 @@ import ReactDOM from "react-dom";
 import App from "../Navbar/Navbar";
 import Hero from "../hero/hero";
 import Cards from "../Cards/card";
+import "./main.css";
+import swimmerImg from "/src/assets/image 12.png";
+import weddingpic from "/src/assets/wedpic.png";
+import bike from "/src/assets/mounbike.png";
 
 var root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
 	<>
 		<App />
 		<Hero />
-		<Cards />
+		<section className="cards-wrapper">
+			<Cards
+				status="SOLD OUT"
+				img="/src/assets/image 12.png"
+				rating="5.0"
+				noOfReviews="(6)"
+				country="USA"
+				cardTitle="Life lessons with katie"
+				price="$136"
+			/>
+			<Cards
+				status="ONLINE"
+				img="/src/assets/wedpic.png"
+				rating="5.0"
+				noOfReviews="(30)"
+				country="USA"
+				cardTitle="Learn wedding photography"
+				price="$125"
+			/>
+			<Cards
+				status="ONLINE"
+				img="/src/assets/mounbike.png"
+				rating="4.8"
+				noOfReviews="(2)"
+				country="USA"
+				cardTitle="Group mountain biking"
+				price="$50"
+			/>
+		</section>
 	</>
 );
