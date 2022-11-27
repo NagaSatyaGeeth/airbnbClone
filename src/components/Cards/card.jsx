313db8a73aa81ef6d4cacfc492a721871aa924fe
Naga Satya Geeth <67518197+Artistole@users.noreply.github.com>
2022-11-28 01:00:34 +0530

    add data

--- src/components/Cards/card.jsx
+++ src/components/Cards/card.jsx
@@ -3,25 +3,38 @@ import "./card.css";
 import star from "/src/assets/Star 1.png";
 
 var cards = (props) => {
+	let statuses;
+	if (props.item.openSpots === 0) {
+		statuses = "SOLD OUT";
+	} else if (props.item.location === "Online") {
+		statuses = "ONLINE";
+	} else {
+		statuses = "Available";
+	}
 	return (
 		<div className="card-placeholder">
-			<div className="card-info">
-				<div className="card-info-desc">{props.status}</div>
-			</div>
+			{props.item.status && (
+				<div className="card-info">
+					<div className="card-info-desc">{statuses}</div>
+				</div>
+			)}
 			<div className="card-pic">
-				<img src={props.img} className="swimmer-pic" />
+				<img
+					src={`src/assets/${props.item.coverImg}`}
+					className="activity-pic"
+				/>
 			</div>
 			<div className="rating">
 				<div className="star-pic">
 					<img src={star} />
 				</div>
-				<div className="total-rating">{props.rating}</div>
-				<div className="no-of-reviews">{props.noOfReviews}</div>
-				<div className="country">{props.country}</div>
+				<div className="total-rating">{props.item.stats.rating}</div>
+				<div className="no-of-reviews">({props.item.stats.reviewCount}) •</div>
+				<div className="country">{props.item.location}</div>
 			</div>
-			<div className="card-title">{props.cardTitle}</div>
+			<div className="card-title">{props.item.cardTitle}</div>
 			<div className="price">
-				{props.price}
+				${props.item.price}
 				<span className="person"> / person</span>
 			</div>
 		</div>
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,46 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "../Navbar/Navbar";
 import Hero from "../hero/hero";
 import Cards from "../Cards/card";
 import "./main.css";
-import swimmerImg from "/src/assets/image 12.png";
-import weddingpic from "/src/assets/wedpic.png";
-import bike from "/src/assets/mounbike.png";
+import Data from "/src/data.js";
+
+const Cardplace = Data.map((item) => {
+	return <Cards key={item.id} item={item} />;
+});
 
 var root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
 	<>
 		<App />
 		<Hero />
-		<section className="cards-wrapper">
-			<Cards
-				status="SOLD OUT"
-				img={swimmerImg}
-				rating="5.0"
-				noOfReviews="(6)"
-				country="USA"
-				cardTitle="Life lessons with katie"
-				price="$136"
-			/>
-			<Cards
-				status="ONLINE"
-				img={weddingpic}
-				rating="5.0"
-				noOfReviews="(30)"
-				country="USA"
-				cardTitle="Learn wedding photography"
-				price="$125"
-			/>
-			<Cards
-				status="ONLINE"
-				img={bike}
-				rating="4.8"
-				noOfReviews="(2)"
-				country="USA"
-				cardTitle="Group mountain biking"
-				price="$50"
-			/>
-		</section>
+		<section className="cards-wrapper">{Cardplace}</section>
 	</>
 );
